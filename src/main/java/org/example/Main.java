a74386b52acd1f9ac8e6d92368ef9a776bc34b48
s.ryzhov <s.a.ryzhov@bk.ru>
2025-03-28 15:11:39 +0300

    Homework5 v.2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,54 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Введите сумму вклада: ");
+        float sum = scan.nextInt();
+        if (sum <= 0) {
+            System.out.println("Вы ввели неверную сумму вклада: " + sum);
+            scan.remove();
+        } else {
+            System.out.println("Вы ввели сумму вклада: " + sum);
+        }
 
-        Car car = new Car("Mercedes", 1985);
-        Car car2 = new Car("BMW", 1999);
-        car.makeBeBe(1985);
-        car2.makeBeBe(1999);
-        car2.model = "Volkswagen";
-        car2.makeBeBe(2010);
+        System.out.print("Введите срок вклада: ");
+        int month = scan.nextInt();
+        if (month <= 0) {
+            System.out.println("Вы ввели неверный срок вклада: " + month);
+            scan.remove();
+        } else {
+            System.out.println("Вы ввели срок вклада: " + month);
+        }
+
+        float percent = 0.07f;
+        float result = sum;
+        // Расчет с использованием for
+        System.out.println("\nРасчет с использованием for:");
+        for (int depositTerm = 1; depositTerm <= month; depositTerm++) {
+            result = result + (result * percent);
+            System.out.println("После " + depositTerm + " месяцев сумма вклада составит " + result);
+        }
+        // Расчет с использованием while
+        result = sum;
+        int depositTerm = 1;
+        System.out.println("\nРасчет с использованием while:");
+        while (depositTerm <= month) {
+            result = result + (result * percent);
+            System.out.println("После " + depositTerm + " месяцев сумма вклада составит " + result);
+            depositTerm++;
+        }
+
+
+
+//        Car car = new Car("Mercedes", 1985);
+//        Car car2 = new Car("BMW", 1999);
+//        car.makeBeBe(1985);
+//        car2.makeBeBe(1999);
+//        car2.model = "Volkswagen";
+//        car2.makeBeBe(2010);
 
 //        Person person1 = new Person("Иван",20);
 //        person1.printPerson();
