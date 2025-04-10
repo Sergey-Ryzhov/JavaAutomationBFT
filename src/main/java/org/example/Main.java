449c5d05442df30017f4b0853bdeaa1a8f75e608
s.ryzhov <s.a.ryzhov@bk.ru>
2025-04-10 11:07:52 +0300

    Homework 9

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,28 @@
 package org.example;
 
-import java.util.Locale;
-import java.util.Scanner;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        Guitar guitar = new Guitar();
-        Piano piano = new Piano();
-        guitar.play();
-        piano.play();
+        Random rand = new Random();
+        int randomNumber = rand.nextInt(3);
+        System.out.println("Случайное число от 0 до 2: " + randomNumber);
+
+        String[] array1 = new String[3];
+        array1[0] = "привет";
+        array1[1] = "ПОКА";
+        array1[2] = "гипербола";
+        switch (randomNumber) {
+            case 0:
+                System.out.println("Слово из массива: " + array1[0]);
+                break;
+            case 1:
+                System.out.println("Слово из массива: " + array1[1]);
+                break;
+            case 2:
+                System.out.println("Слово из массива: " + array1[2]);
+                break;
+        }
+
     }
 }
